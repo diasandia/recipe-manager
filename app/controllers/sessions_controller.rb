a42783e2bf5c36af9c16f57c123502edf855bbd2
jritten <jritten@umich.edu>
2017-09-20 15:24:39 -0700

    debugged user/sessions views w navbar and errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id 
+      redirect_to root_path
     else
       @errors = @user.errors.full_messages
       render 'new'
