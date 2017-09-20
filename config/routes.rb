af174bafebe8840adc0892bab25b6797fa2842a1
Jane <iamjane.gmail.com>
2017-09-20 15:40:57 -0700

    Added controller index, new, show, create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
