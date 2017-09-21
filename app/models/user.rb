6917f64a85e533e6bc3b0538215fc440c5b6c316
Scott M Fulton <scottmfulton@Scotts-MacBook-Pro.local>
2017-09-21 10:35:50 -0700

    take logic out of view and put in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def get_user_recipe_categories
+    categories = []
+    self.recipes.each do |recipe|
+      if !categories.include?(recipe.category)
+        categories << recipe.category
+      end
+    end
+    categories
+  end
+  
   private
 
   def has_password
